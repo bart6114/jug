d9f6ea620b0bdfb20303cbed1a03286bda624fc5
Bart6114 <bartsmeets86@gmail.com>
2016-07-25 10:51:21 +0200

    added res annotating testing

--- tests/testthat/test_multiple_mw_function.R
+++ tests/testthat/test_multiple_mw_function.R
@@ -4,8 +4,7 @@ context("testing get requests")
 
 test_req<-RawTestRequest$new()
 
-
-test_that("The multiple middlewares set up for one GET path",{
+test_that("The multiple middlewares set up for one GET path by annotating the request",{
 
   set_user_id<-function(req, res, err){
     req$set_header("userId", "test")
@@ -22,4 +21,21 @@ test_that("The multiple middlewares set up for one GET path",{
 
 })
 
+
+test_that("The multiple middlewares set up for one GET path by annotating the response",{
+
+  set_user_id<-function(req, res, err){
+    res$set_header("userId", "test")
+  }
+
+  res<-jug() %>%
+    get("/",
+        set_user_id,
+        function(req,res,err) req$get_header("userId")
+    ) %>%
+    process_test_request(test_req$req)
+
+  expect_equal(res$headers$userId, "test")
+
+})
 
