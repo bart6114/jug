2761b91fa26321c243324944369b048bef59d08d
Bart6114 <bartsmeets86@gmail.com>
2016-07-26 12:15:37 +0200

    refactor request header processing

--- R/request.R
+++ R/request.R
@@ -12,9 +12,8 @@ Request<-
             body=NULL,
             attach=function(key, value) self$params[[key]]<-value,
 
-            ## inspired by https://github.com/nteetor/dull/blob/master/R/request.R
-            get_header=function(key) self$headers[[paste0("HTTP_",toupper(key))]],
-            set_header=function(key, value) self$headers[[paste0("HTTP_",toupper(key))]]<-value,
+            get_header=function(key) self$headers[[key]],
+            set_header=function(key, value) self$headers[[key]]<-value,
             add_params=function(named_list) self$params<-c(self$params, named_list),
 
             initialize=function(req){
@@ -22,17 +21,14 @@ Request<-
               self$path<-req$PATH_INFO
               self$method<-toupper(req$REQUEST_METHOD)
 
-
               if(length(req$CONTENT_TYPE)>0) self$content_type<-tolower(req$CONTENT_TYPE)
 
-
               self$body<-paste0(req$rook.input$read_lines(),collapse = "")
-
               self$params<-parse_params(req, self$body, req$QUERY_STRING, self$content_type)
 
-
-
-              self$headers<-as.list(req)
+              header_keys<-Filter(function(x) grepl("^HTTP", x), names(req))
+              self$headers<-as.list(req)[header_keys]
+              names(self$headers)<-Map(function(x) tolower(gsub("^HTTP_", "", x)), names(self$headers))
 
               if(any(tolower(self$get_header("upgrade"))=="websocket")){
                 self$protocol<-"websocket"
--- tests/testthat/test_func_decoration.R
+++ tests/testthat/test_func_decoration.R
@@ -3,13 +3,31 @@ library(jug)
 context("testing function decorator")
 
 
-test_req<-RawTestRequest$new()
-test_req$query_string("?x=test")
 
-test_that("The correct response is returned by a decorated function",{
+
+
+test_that("The correct response is returned by a decorated function (using a querystring value)",{
+  test_req<-RawTestRequest$new()
+  test_req$query_string("?x=test")
+
   my_func<-function(x){x}
 
   res<-jug() %>%
+    get("/", decorate(my_func)) %>%
+    process_test_request(test_req$req)
+
+  expect_equal(res$body, "test")
+
+})
+
+test_that("The correct response is returned by a decorated function (using a header value)",{
+  test_req<-RawTestRequest$new()
+  test_req$set_header("x", "test")
+
+  my_func<-function(x){x}
+
+  res<-
+    jug() %>%
     get("/", decorate(my_func)) %>%
     process_test_request(test_req$req)
 
