9576ab809ccd05a4dc5672f880df51d6147d09b9
Bart6114 <bartsmeets86@gmail.com>
2017-02-14 14:11:19 +0100

    add strict_params to decorate

--- R/decorators.R
+++ R/decorators.R
@@ -2,10 +2,11 @@
 #'
 #' @param func the function to decorate
 #' @param content_type the content type to set on the response object
+#' @param strict_params if TRUE and not all parameters needed for func
 #'
 #' @importFrom utils modifyList
 #' @export
-decorate<-function(func, content_type="text/html"){
+decorate<-function(func, content_type="text/html", strict_params=FALSE){
   # check which parameters the function allows
   args_allowed<-
     names(formals(func))
@@ -14,12 +15,24 @@ decorate<-function(func, content_type="text/html"){
   function(req, res, err){
     res$content_type(content_type)
 
+    # inspect passed params
     passed_params<-
       modifyList(req$params, req$headers)
 
     passed_params$req<-req
     passed_params$res<-res
     passed_params$err<-err
+
+    if(strict_params){
+      # error if not all args requested by function are present
+      args_present <- sapply(args_allowed, function(p) p %in% names(passed_params))
+      if(!all(args_present)){
+
+        err$set(paste0("Requested arguments not provided: ", paste(args_allowed[!args_present], collapse=", ")))
+        return(NULL)
+      }
+    }
+
 
     # drop not requested params from query params
     if(!"..." %in% args_allowed){
