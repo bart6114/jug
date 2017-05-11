ffdf3a0fbc26954695a46b6911ba41ce456d7b48
Bart6114 <bartsmeets86@gmail.com>
2017-05-11 10:59:59 +0200

    separate "path" argument from "root_path" argument

--- tests/testthat/test_static_file_server.R
+++ tests/testthat/test_static_file_server.R
@@ -3,16 +3,50 @@ library(jug)
 context("testing static file server middleware")
 
 test_req<-RawTestRequest$new()
-test_req$path("/")
-
-
 
 test_that("The static file is loaded correctly",{
+  test_req$path("/")
+
   res<-
     jug() %>%
     serve_static_files() %>%
-    simple_error_handler() %>%
+    simple_error_handler_json() %>%
+    process_test_request(test_req$req)
+
+  expect_true(grepl("test", res$body))
+
+  test_req$path("/index.html")
+
+  res<-
+    jug() %>%
+    serve_static_files() %>%
+    simple_error_handler_json() %>%
     process_test_request(test_req$req)
 
   expect_true(grepl("test", res$body))
 })
+
+test_that("The static file is loaded correctly when the middleware is bound to a path",{
+  test_req$path("/path_a/")
+
+  res<-
+    jug() %>%
+    serve_static_files(path="path_a/") %>%
+    simple_error_handler_json() %>%
+    process_test_request(test_req$req)
+
+  expect_true(grepl("test", res$body))
+
+
+  test_req$path("/path_a/index.html")
+  res<-
+    jug() %>%
+    serve_static_files(path="path_a/") %>%
+    simple_error_handler_json() %>%
+    process_test_request(test_req$req)
+
+  expect_true(grepl("test", res$body))
+
+
+})
+
